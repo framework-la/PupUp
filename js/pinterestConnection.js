6c6f650cbf62a9ad32f17576c1f228c732dcab68
Sagar Soni <Sagar Soni>
2017-05-13 16:10:58 -0700

    added index.html and js files to be able to pull random images from the pinterest boards

---
+++ js/pinterestConnection.js
@@ -0,0 +1,36 @@
+pinterestInfo = {
+	boardFirst: '/v1/boards/',
+	pins: '/pins/',
+	token: 'AQg2uO272oUKSN329dCbfLk94kxyFL5LrPNuTyhEAg8UH6A-jAAAAAA',
+	fields: 'id,url,image',
+	pinterestUrl: 'https://api.pinterest.com',
+}
+
+boardsObj = {
+	dog:'frameworkl/cute-dog-board',
+	kitten:'frameworkl/cute-kitten-board',
+	other:'frameworkl/other',
+}
+
+$(document).ready( function () {
+	var keys = Object.keys(boardsObj) //gets the keys for boardObj -> array
+	var randomBoard = Math.floor(Math.random() * keys.length) //finds the index for a random key
+	get_image(boardsObj[keys[randomBoard]]) // example: boardsObj[keys[2]] keys[2] = other
+})
+
+
+function get_image(board) {
+
+	$.ajax({
+		type:"GET",
+		url: pinterestInfo.pinterestUrl + pinterestInfo.boardFirst + board + pinterestInfo.pins,
+		data: {access_token: pinterestInfo.token,
+				fields: pinterestInfo.fields},
+		success: function (data) {
+			var random = Math.floor(Math.random() * data.data.length)
+
+			var imgUrl = data.data[random].image.original.url
+			$("#start").append("<img src=" + imgUrl + ">")
+		}
+	})
+}
